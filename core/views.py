279ad629ddc9be9f812fecdfb8741094944355a4
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-08 14:05:38 +0530

    testing successfull

--- core/views.py
+++ core/views.py
@@ -2,17 +2,30 @@ from django.shortcuts import render
 from core.models import Email_DB
 from django.contrib.auth.models import User
 from core.serializers import EmailSerializer, AuthSerializer, DashboardSerializer
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ai.output import email_output
 from rest_framework.permissions import IsAuthenticated
 
 
-class RegisterAPI(CreateAPIView):
-    permission_classes=[IsAuthenticated]
-    queryset=User.objects.all()
-    serializer_class=AuthSerializer
+class RegisterAPI(APIView):
+    def post(self, request):
+        serial=AuthSerializer(data=request.data)
+        if serial.is_valid():
+            first_name=serial.validated_data['first_name']
+            last_name=serial.validated_data['last_name']
+            username=serial.validated_data['username']
+            email=serial.validated_data['email']
+            password=serial.validated_data['password']
+
+            if User.objects.filter(username=username).exists():
+                return Response({ 'message':'user already exists' })
+            else:
+                User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+                return Response({ 'message':'authentication successfull' })
+        else:
+            return Response({ 'message':'invalid input' })
 
 class DashboardAPI(APIView):
     permission_classes=[IsAuthenticated]
@@ -33,7 +46,8 @@ class DashboardAPI(APIView):
             Email_DB.objects.create(user=self.request.user ,recipent=recipent, purpose=purpose, tone=tone, length=length, output=output)
 
             return Response(output)
-
+        else:
+            return Response({ 'message':'invalid input' })
 
 class EmailDetailAPI(RetrieveAPIView):
     permission_classes=[IsAuthenticated]
